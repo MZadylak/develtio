c7efe8b9ba13a733271c9d36fadcc848e01bc938
Maciek <maciej.zadylak@gmail.com>
2021-02-24 10:39:27 +0100

    footer completed

--- footer.php
+++ footer.php
@@ -19,7 +19,48 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 	<div class="<?php echo esc_attr( $container ); ?>">
 
-		
+		<footer>
+			<div class="container">
+
+				<div class="row">
+
+					<div class="col-lg-3 about">
+						<h4>O nas</h4>
+						<ul>
+							<li>Strona główna</li>
+							<li>O nas</li>
+							<li>Dołącz do nas</li>
+							<li>Zostań Agentem</li>
+							<li>Kariera</li>
+						</ul>
+					</div>
+					
+					<div class="col-lg-3 safety">
+						<h4>Ubezpieczenia</h4>
+						<ul>
+							<li>Ubezpieczenia mienia, nieruchomości</li>
+							<li>Ubezpieczenia osobowe</li>
+							<li>Ubezpieczenia rolne</li>
+							<li>Ubezpieczenie OC</li>
+							<li>Zobacz pełną ofertę</li>
+						</ul>
+					</div>
+					<div class="col-lg-3 help">
+						<h4>Pomoc</h4>
+						<ul>
+							<li>Oddziały/Kontakt</li>
+							<li>Towarzystwo ubezpieczeniowe</li>
+							<li>Przydatne materiały</li>
+							<li>Informacje Administracyjne</li>
+							<li>Polityka cookies</li>
+					</div>
+					<div class="col-lg-3 contact">
+						<h4>Infolinia</h4>
+						<a href="tel:+48223111234"><span>+48 22 311 1234</span></a>
+					</div>
+				</div>
+			</div>
+		</footer>	
 
 	</div><!-- container end -->
 
